0b6c23e2786e0fce2feb9adab5e29879c2c3905a
jamesliu <jamesliu@au1.ibm.com>
2017-09-19 13:35:19 +1000

    add time to Story directive

--- js/app/directives/Story.js
+++ js/app/directives/Story.js
@@ -36,6 +36,18 @@ function StoryController($scope, TopStoriesService) {
 		return domain
 	}
 
+	function getHoursAgo(seconds) {
+		const secondDifference = (Date.now()/1000) - seconds
+
+		if (secondDifference < 3600) {
+			const minutesAgo = secondDifference/60
+			return `${Math.floor(minutesAgo)} minutes ago`
+		} else {
+			const hoursAgo = secondDifference/3600
+			return `${Math.floor(hoursAgo)} hours ago`
+		}
+	}
+
 	TopStoriesService
 		.getStory($scope.id)
 		.then(function(res) {
@@ -46,5 +58,6 @@ function StoryController($scope, TopStoriesService) {
 			vm.domain = getDomainFromUrl(vm.url)			
 			vm.score = vm.story.score
 			vm.author = vm.story.by
+			vm.time = getHoursAgo(vm.story.time)
 		})
 }
