0fa5afbffbe5a46ded524d6b0c94f55836dd8c1d
jamesliu <jamesliu@au1.ibm.com>
2017-09-20 17:06:32 +1000

    add vm.start property to TopStoriesController

--- js/app/controllers/TopStoriesController.js
+++ js/app/controllers/TopStoriesController.js
@@ -6,20 +6,29 @@
 		.controller('TopStoriesController', TopStoriesController)
 	
 	// dependency injection done in this fashion so it doesn't break when minified
-	TopStoriesController.$inject = ['TopStoriesService']
+	TopStoriesController.$inject = ['TopStoriesService', '$scope']
 
-	function TopStoriesController(TopStoriesService) {
+	function TopStoriesController(TopStoriesService, $scope) {
 		var vm = this
 		vm.stories = []
+		vm.start
 
 		activate()
 
 		function activate() {
+			vm.stories = []
+			$scope.updatePage = updatePage
+
 			TopStoriesService
 				.getStories()
 				.then(function(res) {
 					vm.stories = res.data
 				})
+			
+			function updatePage(newPageNumber, oldPageNumber) {
+				vm.start = 30 * (newPageNumber - 1) + 1
+				console.log(vm.start)
+			}
 		}
 	}
 })()
