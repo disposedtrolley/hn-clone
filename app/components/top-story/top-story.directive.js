cf08dfd4fa3b90cd21dad19c667505a7c5d2f848
jamesliu <jamesliu@au1.ibm.com>
2017-09-25 15:01:07 +1000

    added links between top stories state and story state

--- app/components/top-story/top-story.directive.js
+++ app/components/top-story/top-story.directive.js
@@ -1,56 +1,58 @@
 // The Story directive will have an isolate scope; TopStoriesController will pass the ID of each story
 // into this directive.
 
-angular
-	.module('app')
-	.directive('story', story)
+(function() {
+	'use strict'
 
-function story() {
-	const directive = {
-		restrict: 'E',
-		replace: true,
-		scope: {
-			id: '=id'
-		},
-		templateUrl: 'app/components/top-story/top-story.html',
-		controller: TopStoryController,
-		controllerAs: 'vm',
-		link: linkFunc
+	angular
+		.module('app')
+		.directive('story', story)
+	
+	story.$inject = ['TopStoriesService', 'getTimeAgoFilter', 'getDomainFromUrlFilter']
+	function story(TopStoriesService, getTimeAgoFilter, getDomainFromUrlFilter) {
+		const directive = {
+			bindToController: true,
+			controller: StoryDirectiveController,
+			controllerAs: 'vm',
+			link: linkFunc,
+			templateUrl: 'app/components/top-story/top-story.html',
+			restrict: 'E',
+			scope: {
+				storyId: '=id'
+			},
+		}
+	
+		return directive
+
+		function linkFunc(scope, elem, attrs) {
+			const hideLink = elem[0].querySelector('.hide')
+			hideLink.addEventListener('click', function(evt) {
+				elem.parent().remove()
+			})
+	
+			scope.$on('$destroy', function() {
+				elem.off()
+			})
+		}
 	}
 
-	return directive
+	function StoryDirectiveController(TopStoriesService, getTimeAgoFilter, getDomainFromUrlFilter) {
+		let vm = this
 
-	function linkFunc(scope, elem, attrs) {
-		const hideLink = elem[0].querySelector('.hide')
-		hideLink.addEventListener('click', function(evt) {
-			elem.parent().remove()
-		})
-
-		const index = attrs.index
-		scope.index = index
-
-		scope.$on('$destroy', function() {
-			elem.off()
-		})
+		vm.$onInit = function() {
+			TopStoriesService
+				.getStory(vm.storyId)
+				.then(function(res) {
+					vm.story = res.data
+					vm.title = vm.story.title
+					vm.url = vm.story.url
+					if (!vm.url) vm.url = 'https://news.ycombinator.com/item?id=' + vm.story.id
+					vm.domain = getDomainFromUrlFilter(vm.url)			
+					vm.score = vm.story.score
+					vm.author = vm.story.by
+					vm.time = getTimeAgoFilter(vm.story.time)
+					vm.numComments = vm.story.descendants
+				})
+		}
 	}
-}
-
-TopStoryController.$inject = ['$scope', 'TopStoriesService', 'getTimeAgoFilter', 'getDomainFromUrlFilter']
-
-function TopStoryController($scope, TopStoriesService, getTimeAgoFilter, getDomainFromUrlFilter) {
-	let vm = this
-
-	TopStoriesService
-		.getStory($scope.id)
-		.then(function(res) {
-			vm.story = res.data
-			vm.title = vm.story.title
-			vm.url = vm.story.url
-			if (!vm.url) vm.url = 'https://news.ycombinator.com/item?id=' + vm.story.id
-			vm.domain = getDomainFromUrlFilter(vm.url)			
-			vm.score = vm.story.score
-			vm.author = vm.story.by
-			vm.time = getTimeAgoFilter(vm.story.time)
-			vm.numComments = vm.story.descendants
-		})
-}
+})()
